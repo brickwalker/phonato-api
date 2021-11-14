36e6b13dd55d40837056d6bdedb38167988cabdc
brickwalker <artem.nedostup@gmail.com>
2021-11-14 12:32:21 +0000

    add router

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
-console.log("from typescript 222");
+import express from "express";
+import router from "./components/router";
+
+const app = express();
+const port: number = 3000;
+const rootEndpoint: string = "nato";
+
+app.use(`/${rootEndpoint}`, router);
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}/${rootEndpoint}`);
+});
